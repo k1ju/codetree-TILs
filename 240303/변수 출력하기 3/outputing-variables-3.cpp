b19593a159f84fc69f3cfc22a8c389e151701ced
k1ju <gksrlwn98@naver.com>
2024-03-03 20:09:24 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240303/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a = 1, b = 2;
+    string c = "C";
+    cout << a << "->" << b << "->" << c;
+
+    return 0;
+}
