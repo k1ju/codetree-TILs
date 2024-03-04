71a96e1e8634c61521e79dd3678a4a18477256a1
k1ju <gksrlwn98@naver.com>
2024-03-04 18:36:49 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240304/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double ft = 30.48;
+    double a;
+    cin >> a;
+
+    cout << fixed;
+    cout.precision(1);
+    cout << a * ft;
+    
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
