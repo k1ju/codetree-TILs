b7e95f8a55c561e28157231035f3d7bd232f821f
k1ju <gksrlwn98@naver.com>
2024-03-04 18:47:47 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240304/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+    cout << fixed;
+    cout.precision(2);
+    double a,b;
+    cin >> a >> b;
+    cout << a+b;
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
