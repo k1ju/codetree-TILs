3bec44d8fad2b182e58e4c389a7fad4b6b19d62d
k1ju <gksrlwn98@naver.com>
2024-03-04 18:49:06 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240304/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double a,b,c;
+    cout << fixed;
+    cout.precision(3);
+    cin >> a >> b >> c;
+    cout << a << endl << b << endl << c;
+    
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
