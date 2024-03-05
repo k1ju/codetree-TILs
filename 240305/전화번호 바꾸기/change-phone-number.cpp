494a8f4a2a935c0b66807f6338d2cd71b17e5aef
k1ju <gksrlwn98@naver.com>
2024-03-05 13:06:15 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240305/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+
+
+int a,b,c;
+char d;
+
+cin >> a >> d >> b >> d >> c;
+cout << "010" << d << c << d << b;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
