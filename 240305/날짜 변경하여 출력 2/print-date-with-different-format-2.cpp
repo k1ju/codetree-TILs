21da07272025f04477f0affe8ee1c24d258d56cd
k1ju <gksrlwn98@naver.com>
2024-03-05 12:54:30 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240305/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int a,b,c;
+char d;
+
+cin >> a >> d >> b >> d >> c;
+cout << c << "." << a << "." << b;
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
