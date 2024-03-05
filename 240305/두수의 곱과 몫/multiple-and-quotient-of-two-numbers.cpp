18cd399d39982f1c1668527e7742bac4a6b6c098
k1ju <gksrlwn98@naver.com>
2024-03-05 14:22:48 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
+++ 240305/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -6,8 +6,10 @@ int main() {
 int a,b;
 
 cin >> a >> b;
-cout << "300 * 5 = " << a*b << endl;
-cout << "300 / 5 = " << a/b << endl;
+cout << a << " * " << b << " = " << a*b << endl;
+cout << a << " / " << b << " = " << a/b << endl;
+
+
 
     // 여기에 코드를 작성해주세요.
     return 0;
