92d3584a9ea359ac7dcc1e1ea1c5af3baedfd056
k1ju <gksrlwn98@naver.com>
2024-03-06 15:47:58 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a,b;
+    int c;
+    cin >> a >> b;
+
+    c = a>b ? a : b;
+    cout << c;
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
