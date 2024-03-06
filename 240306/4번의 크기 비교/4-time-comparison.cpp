1c29537f3864b4fb4341d8526c5a08a7cd006a4c
k1ju <gksrlwn98@naver.com>
2024-03-06 17:26:06 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240306/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a,b,c,d,e;
+
+    if(a>b){
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    cout << endl;
+    if(a>c){
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    cout << endl;
+    if(a>d){
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    cout << endl;
+    if(a>e){
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    cout << endl;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
