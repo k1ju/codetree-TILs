533c57c3cad0fa1b301c4f27b625f288a8365943
k1ju <gksrlwn98@naver.com>
2024-03-06 16:02:04 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240306/물의 상태/state-of-water.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+int n;
+cin >> n;
+string c;
+
+if(n<0){
+ c = "ice";
+} else if(n>100){
+    c = "vapor";
+} else {
+    c = "water";
+}
+cout << c;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
