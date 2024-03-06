af1b73c3d51eddfe148ccd94f9e98c34a300d07f
k1ju <gksrlwn98@naver.com>
2024-03-06 17:01:27 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/두 번의 연산/two-operations.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+    int a;
+    cin >> a;
+
+    if((a%2) == 1){
+        a+=3;
+    }
+
+
+
+    if((a%3) == 0){
+        a/=3;
+    }
+
+    cout << a;
+    
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
