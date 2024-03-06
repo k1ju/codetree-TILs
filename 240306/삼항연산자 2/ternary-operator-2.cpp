c3b61929bda75b98978e44fa65d21bfbb884229a
k1ju <gksrlwn98@naver.com>
2024-03-06 15:58:49 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+
+
+int a;
+cin >> a;
+
+string c;
+c = (a==1) ? "t" : "f";
+
+cout << c;
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
