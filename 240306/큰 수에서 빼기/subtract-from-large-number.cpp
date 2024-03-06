5db2a060cf21c03d26838713bd04f47a872d9010
k1ju <gksrlwn98@naver.com>
2024-03-06 15:26:22 +0900

    Title: 큰 수에서 빼기, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/큰 수에서 빼기/subtract-from-large-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+int a,b;
+
+cin >> a >> b;
+int result;
+
+if(a>b){
+    result = a-b;
+} else {
+    result = b-a;
+}
+
+cout << result;
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
