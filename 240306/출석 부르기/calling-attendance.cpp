efb62a84cffe0fb34b194d6e0eea30691bad3803
k1ju <gksrlwn98@naver.com>
2024-03-06 16:15:59 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/출석 부르기/calling-attendance.cpp
+++ 240306/출석 부르기/calling-attendance.cpp
@@ -7,13 +7,15 @@ int main() {
 
     int number;
     cin >> number;
-    
+
     if(number==1){
         cout << "John";
     } else if(number == 2){
         cout << "Tom";
     } else if (number == 3){
         cout << "Paul";
+    } else{
+        cout << "Vacancy";
 
     }
     return 0;
