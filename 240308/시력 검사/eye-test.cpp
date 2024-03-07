a72891c11df772398c17c444f7b36a6fe3d6deed
k1ju <gksrlwn98@naver.com>
2024-03-08 00:36:01 +0900

    Title: 시력 검사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240308/시력 검사/eye-test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a,b;
+    cin >> a >> b;
+    if((a>=1) && (b>=1)){
+        cout << "High";
+    }else if((a>=0.5) && (b>=0.5)){
+        cout << "Middle";
+    } else {
+        cout << "Low";
+
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
