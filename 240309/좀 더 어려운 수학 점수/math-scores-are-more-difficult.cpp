a80bb93d7090d0dc6930721153a638ae21ac5f67
k1ju <gksrlwn98@naver.com>
2024-03-09 13:43:19 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240309/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 240309/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -4,8 +4,8 @@ int main() {
 
     int a_math, a_eng;
     int b_math, b_eng;
-    cin << a_math << a_eng;
-    cin << b_math << b_eng;
+    cin >> a_math >> a_eng;
+    cin >> b_math >> b_eng;
 
     if((a_math == b_math)){
         if(a_eng>b_eng){
