68232a22cbc324db79e1c56c8e3d298101730637
k1ju <gksrlwn98@naver.com>
2024-03-19 21:19:34 +0900

    Title: 문자 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240319/문자 출력하기/print-text.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    char i;
+    cin >> i;
+
+    for(int n=0; n<8; n++){
+        cout << i;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
