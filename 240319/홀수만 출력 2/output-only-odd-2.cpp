5e3ae0096203b7ac028ec72deff58f475d6f38c7
k1ju <gksrlwn98@naver.com>
2024-03-19 13:29:17 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/홀수만 출력 2/output-only-odd-2.cpp
+++ 240319/홀수만 출력 2/output-only-odd-2.cpp
@@ -2,15 +2,25 @@
 using namespace std;
 int main() {
 
-    int a,b;
+    int a, b;
+    cin >> b >> a;
 
-    cin >> a >> b;
+    // for(b; b>a; b--){
+    //     if(b%2 == 1){
+    //         cout << b << " ";
+    //     }
+    //     cout << b << " ";
+    // }
 
-    for(b;b>a;b--){
-        if(b%2 == 0){
-            cout << b << " ";
+
+    for(int i=b; i>=a; i--){
+        if(i%2 == 1){
+            cout << i << " ";
         }
     }
+
+
+
     // 여기에 코드를 작성해주세요.
     return 0;
 }
