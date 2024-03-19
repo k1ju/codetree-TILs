a458844786c62601204f092271a108d5f53cf0c9
k1ju <gksrlwn98@naver.com>
2024-03-19 12:59:34 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240319/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a >> b;
+
+    for(a; a<=b; a+=2){
+        cout << a << " ";
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
