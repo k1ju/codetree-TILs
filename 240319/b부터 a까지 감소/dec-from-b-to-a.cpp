e49b83c57b0f6227cc7ca67bea07ecbeeeb29fb0
k1ju <gksrlwn98@naver.com>
2024-03-19 13:05:34 +0900

    Title: b부터 a까지 감소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240319/b부터 a까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int a,b;
+    cin >> a >> b;
+
+    for(b; b>a-1;b-- ){
+        cout << b << " ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
