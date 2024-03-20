641eecd52c833a0371099ba86d6f3b831e95839b
k1ju <gksrlwn98@naver.com>
2024-03-20 20:23:26 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240320/a 부터 b 까지/a-to-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a >> b;
+    while(a<=b){
+        cout << a << " ";
+
+        if(a%2 ==0){
+            a+=3;
+        } else {
+            a*=2;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
