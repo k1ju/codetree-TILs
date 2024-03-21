4e2aef663eda0e24e557502f783b9f7ed54e3135
k1ju <gksrlwn98@naver.com>
2024-03-21 20:34:00 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240321/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int cnt =0;
+    
+
+    for(int i=0; i<5; i++){
+        int n;
+        cin >> n;
+        if(n%2==0){
+            cnt ++;
+        }
+    }
+
+    cout << cnt;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
