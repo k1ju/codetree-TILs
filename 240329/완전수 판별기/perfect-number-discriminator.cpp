c5b419f91ab406ea55ce601f69bfcc4e4a5a683e
k1ju <gksrlwn98@naver.com>
2024-03-29 13:03:01 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240329/완전수 판별기/perfect-number-discriminator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n;
+    cin >> n;
+    int sum =0;
+
+    for(int i=1; i < n; i++){
+        if (n%i ==0){
+            sum += i;
+        }
+    }
+
+    if(sum == n){
+        cout << "P";
+    } else {
+        cout << "N";
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
