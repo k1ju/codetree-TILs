29cf4a75b395e553f9e1b975b268cbeb6604267e
k1ju <gksrlwn98@naver.com>
2024-03-29 13:38:44 +0900

    Title: 1까지 나누기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240329/1까지 나누기/divide-by-1.cpp
+++ 240329/1까지 나누기/divide-by-1.cpp
@@ -7,13 +7,15 @@ int main() {
     int sum = n;
     int cnt = 0;
 
-    for(int i=0; i<=n; i++){
+    for(int i=1; i<=n; i++){
         if(sum < 1){
             break;
         }
-        sum = n%i;
+        sum = sum / i;
         cnt ++;
     }
+    cout << cnt;
+
     // 여기에 코드를 작성해주세요.
     return 0;
 }
