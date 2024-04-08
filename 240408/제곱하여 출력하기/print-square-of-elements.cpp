0159a45b3e56e9bc17d1d890bb1b80270c3ebfe8
k1ju <gksrlwn98@naver.com>
2024-04-08 14:02:09 +0900

    Title: 제곱하여 출력하기, Time: 17ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240408/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+int list[10];
+    for(int i=0; i< n; i++) {
+        cin >> list[i];
+    }
+
+for(int i=0; i< n; i++){
+    
+    cout << pow(list[i],2) << " ";
+
+}
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
