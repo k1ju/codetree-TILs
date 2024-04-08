1df1f0706c39962d9669109963aa0382908f4263
k1ju <gksrlwn98@naver.com>
2024-04-08 14:04:51 +0900

    Title: 제곱하여 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/제곱하여 출력하기/print-square-of-elements.cpp
+++ 240408/제곱하여 출력하기/print-square-of-elements.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -7,14 +7,14 @@ int main() {
     int n;
     cin >> n;
 
-int list[5];
+int list[100];
     for(int i=0; i< n; i++) {
         cin >> list[i];
     }
 
 for(int i=0; i< n; i++){
     
-    cout << list[i] * list[i] << " ";
+    cout << pow(list[i],2) << " ";
 
 }
     // 여기에 코드를 작성해주세요.
