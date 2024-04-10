b838f089febd2ba3979ddcc0d175b8108e1ccbf7
k1ju <gksrlwn98@naver.com>
2024-04-10 13:49:15 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240410/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[100];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i=1; i< 10; i++){
+        int cnt = 0;
+        for(int j=0; j<n; j++){
+            if(i == arr[j]){
+                cnt++;
+            }
+        }
+        cout << cnt << endl;
+
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
