c3efc8983bfa76682e529701ac9f18cef33a49c5
k1ju <gksrlwn98@naver.com>
2024-04-19 13:47:27 +0900

    Title: 문자열 범위 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240419/문자열 범위 출력하기/print-string-in-range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    string str;
+    getline(cin, str);
+
+    for(int i=2; i<=9; i++){
+        cout << str[i];
+
+    }
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
