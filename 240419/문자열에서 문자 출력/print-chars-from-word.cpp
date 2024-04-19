824bddebfb6aa985244730d6ad7c6a0d54c22af4
k1ju <gksrlwn98@naver.com>
2024-04-19 14:49:33 +0900

    Title: 문자열에서 문자 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240419/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    string str;
+    getline(cin, str);
+
+    for(int i=0; str[i] != '\0'; i++){
+
+        cout << str[i] << endl;
+
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
