dd71e81cdc52127f6b105aee22ca47d8a8631053
k1ju <gksrlwn98@naver.com>
2024-09-25 13:45:51 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240925/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
+++ 240925/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
@@ -3,36 +3,25 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
 
         String[] arr = new String[11];
 
         Scanner sc = new Scanner(System.in);
-        boolean isOk = false;
+        int count = 0;
 
         for(int i=0; i<11; i++){
             arr[i] = sc.nextLine();
         }
 
-        ArrayList<String> list = new ArrayList<>();
-
         for(int i=0; i<10; i++){
             if(arr[i].charAt(arr[i].length()-1) == arr[10].charAt(0)){
-                isOk = true;
-                list.add(arr[i]);
+                System.out.println(arr[i]);
+                count++;
             }    
         }
-
-        if(isOk == false){
-            System.out.print("None");
-            return;
+        if(count == 0){
+            System.out.println("None");
         }
 
-
-        for(int i=0; i<list.size(); i++){
-
-            System.out.println(list.get(i));
-
-        }
     }
 }
