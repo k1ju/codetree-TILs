3e6ccf79de5d074d0d2d7d673077207afafa5850
k1ju <gksrlwn98@naver.com>
2024-10-02 21:46:36 +0900

    Title: 정수 명령 처리 5, Time: 436ms, Memory: 23MB, Status: Passed - CodeTree

---
+++ 241002/정수 명령 처리 5/process-numeric-commands-5.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = Integer.parseInt(sc.next());
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for(int i=0; i<= num; i++){
+            String[] str = sc.nextLine().split(" ");
+
+            if( str[0].equals("push_back") ){
+                list.add(str[1]);
+            } else if( str[0].equals("get") ){
+                System.out.println(list.get(Integer.parseInt(str[1])-1));
+            } else if( str[0].equals("size") ){
+                System.out.println(list.size());
+            } else if ( str[0].equals("pop_back") ) {
+                list.remove(list.size()-1);
+            }
+        }
+    }
+}
