26b7f006ee53c481d489a98187c8e85fd8f6650a
k1ju <gksrlwn98@naver.com>
2024-10-05 17:36:53 +0900

    Title: Time to Time, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241005/Time to Time/time-to-time.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = Integer.parseInt(sc.next());
+        int b = Integer.parseInt(sc.next());
+        int c = Integer.parseInt(sc.next());
+        int d = Integer.parseInt(sc.next());
+
+        System.out.print((c*60+d) - (a*60+b));
+
+    }
+}
