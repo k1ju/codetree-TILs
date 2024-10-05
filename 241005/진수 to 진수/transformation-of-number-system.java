08c13ae4028b11b3f92ea6cc6c61387af8619cfc
k1ju <gksrlwn98@naver.com>
2024-10-05 19:38:44 +0900

    Title: 진수 to 진수, Time: 109ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241005/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] str = sc.nextLine().split(" "); 
+
+        int c = Integer.parseInt(sc.nextLine());
+
+        int sum = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(c).reverse();
+
+
+        for(int i=0; i<sb.length(); i++ ){
+
+            sum += Integer.parseInt(String.valueOf(sb.charAt(i))) * Math.pow(8,i);
+
+        }
+        
+        StringBuilder sb2 = new StringBuilder();
+
+        while(sum>0){
+
+            sb2.append( sum % Integer.parseInt(str[1]) );
+
+            sum /= Integer.parseInt(str[1]);
+            
+        }
+
+        System.out.println(sb2.reverse().toString());
+
+
+
+
+
+
+    }
+}
